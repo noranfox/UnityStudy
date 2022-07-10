5ec25b6862b11794550998ee4142cfe3e7715bff
noranfox <sadpo5660@gmail.com>
2022-07-10 20:32:17 +0900

    22/07/10 UnityStudy2

--- Unity Command Study/Assets/Script/_3_BaseImformations.cs
+++ Unity Command Study/Assets/Script/_3_BaseImformations.cs
@@ -27,6 +27,56 @@ public class _3_BaseImformations : MonoBehaviour
         4.Physics Material  탄성과 마찰을 다루는 물리적인 재질 적용하면 Collider에 들어간다
             bounciness -> 탄력성(0~1) bouncecoimbine->다음 바운스는 얼마나 팅길거냐
             friction -> 마찰력
-         */
+         
+         
+    //재귀함수 함수가 자기자신을 다시 호출한다
+    void Think()
+    {
+        nextMove = Random.Range(-1, 2);
+
+        Invoke("Think", 5);
+    }
+
+        //레이함수
+         Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+         RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Floor"));
+            if (rayHit.collider == null)
+            {
+                nextMove *= -1;
+            CancelInvoke();
+            Invoke("Think", 5);
+        }
+         
+        Invoke("Think", 5); -> ""안의 메서드를 뒤에 숫자만큼 시간이 흐른뒤 실행시킨다.
+
+        
+
+        Time.Scale ==> 게임 시간을 멈출수있음. 시간 배속임. 0이면 0배속이므로 정지 1이므로 1배속
+
+        먹은 아이탬마다 편하게 점수관리함수만들기
+        ex
+        
+
+         *//* 
+        void oncollison (collision collision)
+        {
+            if(collision.gameObject.tag == "Item")
+            {
+                bool isBronze = collision.gameObject.name.Contains("Bronze");
+                bool isSilver = collision.gameObject.name.Contains("Silver");
+                bool isGold = collision.gameObject.name.Contains("Gold");
+                
+                switch
+                    case isBronze
+                    gameMAnager.stagepoint += 50;
+                    break;
+                    case isSilver:
+                    gameManager.stagepoint += 100;
+                    break;
+                    case isGold
+                    gameManager.stagepoint += 150;
+                    break;
+                }
+        } */
     }
 }
